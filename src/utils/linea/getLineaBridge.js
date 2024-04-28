a65d86929ca093da4b376a520cbfbffc9a2de453
luoye <luoyeETH@gmail.com>
2024-04-28 17:38:18 +0800

    bugfix 修复linea官方桥统计问题

--- src/utils/linea/getLineaBridge.js
+++ src/utils/linea/getLineaBridge.js
@@ -4,7 +4,7 @@ import { ethers } from 'ethers';
 
 async function getLineaBridge(address, apiKey) {
     try {
-        let url = `https://api.lineascan.build/api?module=account&action=txlistinternal&address=${address}&startblock=0&endblock=2702578&sort=asc&apikey=${apiKey}`;
+        let url = `https://api.lineascan.build/api?module=account&action=txlistinternal&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${apiKey}`;
         const response = await axios.get(url);
         const transactions = response.data.result;
         const bridgeTx = transactions.filter(item => item.from === "0x508ca82df566dcd1b0de8296e70a96332cd644ec" && item?.value > 300000000000000);
